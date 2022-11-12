52c8904b3cb4150abbda8567b1804f953630f7dd
quetzo <hojitabrillante@gmail.com>
2022-11-12 11:09:31 -0600

    commit que no habia hecho de las lecciones pasadas

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,11 @@
 class WelcomeController < ApplicationController
   def hello
+    @variable = "como estas "
+    @datos = {:name => User.first.name, :age => User.first.age}
   end
   
   def hola
+    @credentials = Rails.application.credentials.hello
     @x = "hola"
 
   end
